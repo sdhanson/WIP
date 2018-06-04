6a2586337a9f4a66fb21283fa34805a3b08ab46f
Sara Hanson <sdhanson@usc.edu>
2018-06-04 11:38:32 -0700

    Adding plots for each OCULUS GO walking script

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ BIN_DIR = bin
 CXX = g++
 CPPFLAGS = -g -Wall -Wextra -Wconversion --std=c++11
 
-all: change ochange
+all: change ochange oplot
 
 # comp.o: comp.cpp comp.h bitstr.h
 # 	$(CXX) $(CPPFLAGS) -c $< -o $@
@@ -11,6 +11,9 @@ change: change.cpp
 	$(CXX) $(CPPFLAGS) $< -o $@
 
 ochange: ochange.cpp
+	$(CXX) $(CPPFLAGS) $< -o $@
+
+oplot: oplot.cpp
 	$(CXX) $(CPPFLAGS) $< -o $@
 
 .PHONY: clean
--- oplot.cpp
+++ oplot.cpp
@@ -15,288 +15,118 @@ string colNames[] = {"Date", "Time", "Rand1", "Rand2", "letter", "Unity", "colon
 	"Acceleration marker", "accel.x", "accel.y", "accel.z", "Euler marker",
 	"euler.x", "euler.y", "euler.z"};
 
-void gyroPairs(ifstream& input) {
+
+void time(ifstream& input, vector<double>& t) {
+
 	// declaring stringstream variables
 	string line, word;
-
-	// initializing column val of gyro.userAcceleration.y
-	int yCol = 5;
-
-	// inititalizing column val of gyro.userAcceleration.z
-	int zCol = 6;
-
-	// vector of yz pairs
-	vector<std::pair<int, int> > pairs;
-
-
-	// takes in each line, reads garbage numbers up to column number
-	// takes the column we want and compares to prev max/min and resets
-	// if need be
-	while(getline(input, line)) {
-		stringstream ss;
-
-		// discarding garbage values
-		for(int i=0; i<yCol; i++) {
-			string temp;
-			ss << line;
-			ss >> temp;
-		}
-
-		// converting y column string to int
-		ss << line;
-		ss >> word;
-		stringstream tt;
-		tt << word;
-		int y;
-		tt >> y;
-
-		// reading in values until z column (useless rn bc
-		// in my program they are always 1 away)
-		for(int i=0; i < (zCol-yCol-1); i++) {
-			string temp;
-			ss << line;
-			ss >> temp;
-		}
-
-		// converting z column string to int
-		ss << line;
-		ss >> word;
-		stringstream zz;
-		zz << word;
-		int z;
-		zz >> z;
-
-		// push into pair
-		pairs.push_back(make_pair(y, z));
-	}
-
-	// can be used to plot
-
-}
-
-// finds max/min magnitudes & line number
-void absMaxMin(ifstream& input, int col, ofstream& output) {
-	// declaring stringstream variables
-	string line, word;
-
-	// initializing the max/min/lineNum variables
-	double max = 0;
-	double min = 1000000;
-	int lineNum = 1;
-	int lineMax, lineMin;
-
-	// takes in each line, reads garbage numbers up to column number
-	// takes the column we want and compares to prev max/min and resets
-	// if need be
-	while(getline(input, line)) {
-		stringstream ss;
-
-		// discarding garbage values
-		for(int i=0; i<col; i++) {
-			string temp;
-			ss << line;
-			ss >> temp;
-		}
-
-		// converting column string to double
-		ss << line;
-		ss >> word;
-		stringstream tt;
-		tt << word;
-		double gyro_y;
-		tt >> gyro_y;
-
-		// converting from 360 to 180 so 359 --> 1 is now -1 --> 1
-		// only for eulerangle columns
-		if(col >= 12) {
-			if((gyro_y > 180) || (gyro_y < -180)) {
-				gyro_y -= 360;
-			}
-		}
-
-		// comparisons to reset max/min and line nums
-		if(abs(gyro_y) > max) {
-			max = abs(gyro_y);
-			lineMax = lineNum;
-		}
-		if(abs(gyro_y) < min) {
-			if(gyro_y > 0.00001) {
-				min = abs(gyro_y);
-				lineMin = lineNum;
-			}
-		}
-		lineNum++;
-	}
-
-	// printing data to output
-	output << "ABSOLUTE VALUE MAX/MIN" << endl;
-	output << "Max: " << max << " line: " << lineMax << endl;
-	output << "Min: " << min << " line: " << lineMin << endl;
-	output << endl;
-}
-
-// finds max/min acceleration & line number
-void signMaxMin(ifstream& input, int col, ofstream& output) {
-	// declaring stringstream variables
-	string line, word;
-
-	// initializing the max/min/lineNum variables
-	double max = 0;
-	double min = 1000000;
-	int lineNum = 1;
-	int lineMax, lineMin;
-
-	// takes in each line, reads garbage numbers up to column number
-	// takes the column we want and compares to prev max/min and resets
-	// if need be
-	while(getline(input, line)) {
-		stringstream ss;
-
-		// discarding garbage values
-		for(int i=0; i<col; i++) {
-			string temp;
-			ss << line;
-			ss >> temp;
-		}
-
-		// converting column string to double
-		ss << line;
-		ss >> word;
-		stringstream tt;
-		tt << word;
-		double gyro_y;
-		tt >> gyro_y;
-
-		// converting from 360 to 180 so 359 --> 1 is now -1 --> 1
-		// only for eulerangle columns
-		if(col >= 12) {
-			if((gyro_y > 180) || (gyro_y < -180)) {
-				gyro_y -= 360;
-			}
-		}
-
-		// comparisons to reset max/min and line nums
-		if(gyro_y >= max) {
-			max = gyro_y;
-			lineMax = lineNum;
-		}
-		if(gyro_y <= min) {
-			if(gyro_y != 0) {
-				min = gyro_y;
-				lineMin = lineNum;
-			}
-		}
-		lineNum++;
-	}
-
-	// printing data to output
-	output << "NON ABSOLUTE VALUE MAX/MIN" << endl;
-	output << "Max: " << max << " line: " << lineMax << endl;
-	output << "Min: " << min << " line: " << lineMin << endl;
-	output << endl;
-}
-
-void change(ifstream& input, int col, ofstream& output) {
-	// declaring stringstream variables
-	string line, word;
-
-	// initializing max/min/lineNum variables
-	double max = 0;
-	double min = 1000000;
-	int lineNum = 1;
-	int lineMax, lineMin;
-
-	// initializing prev (used to calc change)
-	double prev = 0;
-
-	// initializing avg and count (used to divide total by total count to get avg)
-	double avg = 0;
+	double prev;
 	int count = 0;
+	double sum = 0;
 
 	// takes in each line, reads garbage numbers up to column number
-	// takes the column we want and calcs abs value change bc prev and curr num
-	// if need be resets max/min change and line numbers and adds to total avg
+	// takes the column we want and compares to prev max/min and resets
+	// if need be
 	while(getline(input, line)) {
 		stringstream ss;
 
-		// discarding garbage variables
+		// discarding garbage values
+		for(int i=0; i<1; i++) {
+			string temp;
+			ss << line;
+			ss >> temp;
+		}
+
+		// converting column string to double
+		ss << line;
+		ss >> word;
+		stringstream tt;
+		tt << word;
+		string val;
+		tt >> val;
+
+		stringstream rr;
+		rr << val;
+		char temp;
+		rr >> temp;
+		int h = 0;
+		while((temp != ':') || (h != 2)) {
+			rr >> temp;
+			if(temp == ':') {
+				h++;
+			}
+		}
+
+		double curr;
+		rr >> curr;
+
+		double diff;
+
+		if(count == 0) {
+			prev = curr;
+			diff = 0;
+			count++;
+		} else {
+			diff = curr-prev;
+			sum += diff;
+			prev=curr;
+		}
+
+		t.push_back(sum);
+	}
+	
+}
+
+void y(ifstream& input, vector<double>& y, int col) {
+	// declaring stringstream variables
+	string line, word;
+
+	// takes in each line, reads garbage numbers up to column number
+	// takes the column we want and compares to prev max/min and resets
+	// if need be
+	while(getline(input, line)) {
+		stringstream ss;
+
+		// discarding garbage values
 		for(int i=0; i<col; i++) {
 			string temp;
 			ss << line;
 			ss >> temp;
 		}
 
-		// converting column string to number
+		// converting column string to double
 		ss << line;
 		ss >> word;
 		stringstream tt;
 		tt << word;
-		double gyro_y;
-		tt >> gyro_y;
+		double val;
+		tt >> val;
 
-		// converting from 360 to 180 so 359 --> 1 is now -1 --> 1
-		// only for eulerangle columns
-		if(col >= 12) {
-			if((gyro_y > 180) || (gyro_y < -180)) {
-				gyro_y -= 360;
-			}
-		}
-
-		// calculates abs change and manages avg variables
-		// ignore first line bc starting point (prev is default 0)
-		if(lineNum != 1) {
-		double change = abs(abs(gyro_y) - prev);
-		avg += change;
-		count++;
-
-			// resets max/min/line num if need be
-			if(change > max) {
-				max = change;
-				lineMax = lineNum;
-			}
-			if(change < min) {
-				if(change != 0) {
-					min = change;
-				}
-				lineMin = lineNum;
-			}
-		}
-
-		// resets prec
-		prev = abs(gyro_y);
-		lineNum++;
+		y.push_back(val);
 	}
+}
 
-	// calculates avg
-	avg = avg/count;
-
-	// outputs data
-	output << "ABSOLUTE VALUE ONE STEP CHANGE" << endl;
-	output << "Max change: " << max << " line: " << lineMax << endl;
-	output << "Min change: " << min << " line: " << lineMin << endl;
-	output << "Average change: " << avg << endl;
-	output << endl;
+void format(ofstream& output, vector<double>& v) {
+	output << "Length: " << v.size() << endl;
+	output << "[";
+	for(unsigned int i=0; i<v.size(); i++) {
+		if(i == v.size()-1) {
+			output << v[i] << "]" << endl;
+		} else {
+			output << v[i] << ", ";
+		}
+	}
 }
 
 
-int main(int argc, char* argv[]) {
-	// checking inputs & printing desired format if incorrect num
-	if(argc < 2) {
-		cout << "EXEC COL" << endl;
-	}
-
-	// col number we want to start analysis at
-	stringstream colNum;
-	colNum << argv[1];
-	int col;
-	colNum >> col;
-
-
+int main() {
 	// creating array of file names so when one thing is changed in .cpp
 	// we can change all of the output files at once
-	int SIZE = 11;
-	string file[SIZE] = {"oevery.txt", "osit1.txt", "osit2.txt", "osit3.txt", "osit4.txt", 
-						"owalk1.txt", "owalk2.txt", "owalk3.txt", "owalk4.txt", "oud.txt", "olr.txt"};
+	// int SIZE = 11;
+	int SIZE = 1;
+	string file[SIZE] = {"owalk4.txt"};
+	// string file[SIZE] = {"oevery.txt", "osit1.txt", "osit2.txt", "osit3.txt", "osit4.txt", 
+	// 					"owalk1.txt", "owalk2.txt", "owalk3.txt", "owalk4.txt", "oud.txt", "olr.txt"};
 
 	// creates dynamic array of input and output file paths
 	string* inputs = new string[SIZE];
@@ -305,36 +135,50 @@ int main(int argc, char* argv[]) {
 	// fills in arrays with file paths
 	for(int i=0; i<SIZE; i++) {
 		string ipath = "./odata/" + file[i];
-		string opath = "./oanalysis/O" + file[i];
+		string opath = "./panalysis/P" + file[i];
 		inputs[i] = ipath;
 		outputs[i] = opath;
 	}
+
+	vector<double> times;
+	vector<double> ys;
+	vector<double> xs;
+	vector<double> zs;
 
 	// iterates through all files and performs the analysis
 	for(int j=0; j<SIZE; j++) {
 	// iterating through columns from col input --> final column
 		ofstream output(outputs[j]);
-		for(int i=col; i<15; i++) {
-			if(i != 11) {
 
-				output << "COL: " << colNames[i] << endl;
-				ifstream input(inputs[j]);
-				absMaxMin(input, i, output);
+		ifstream input(inputs[j]);
+		cout << "HEREEEEEEE" << endl;
+		time(input, times);
 
-				ifstream input2(inputs[j]);
-				signMaxMin(input2, i, output);
+		ifstream input1(inputs[j]);
+		y(input1, ys, 9);
 
-				ifstream input3(inputs[j]);
-				change(input3, i, output);
-				output << endl;
-			}
-		}
+		ifstream input2(inputs[j]);
+		y(input2, xs, 8);
+
+		ifstream input3(inputs[j]);
+		y(input3, zs, 10);
+
+		output << "TIME VECTOR" << endl;
+		format(output, times);
+
+		output << "X VECTOR" << endl;
+		format(output, xs);
+
+		output << "Y VECTOR" << endl;
+		format(output, ys);
+
+		output << "Z VECTOR" << endl;
+		format(output, zs);
 	}
 
 	// frees memory
 	delete [] inputs;
 	delete [] outputs;
-
 
 	return 0;
 }
