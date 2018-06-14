2c78295fbee82a6d96549142fdc2d48e5daecd33
Sara Hanson <sdhanson@usc.edu>
2018-06-14 07:56:56 -0700

    initial pass at larger analysis file don't know if it will actually work

--- analysis.cpp
+++ analysis.cpp
@@ -156,6 +156,7 @@ void format(ofstream& output, vector<double>& v) {
 }
 /* ************** PYTHON HELPERS END ***************** */
 
+/* PYTHON fUNCTION TO MANAGE SETTING UP THE PYTHON FILE */
 void python(bool oculus, int ycol, ofstream& output, ifstream& input) {
 	vector<double> times;
 	vector<double> ys;
@@ -173,8 +174,239 @@ void python(bool oculus, int ycol, ofstream& output, ifstream& input) {
 
 	output << "y = ";
 	format(output, ys);
+}
+/* *************************************************** */
+
+
+
+/* THRESHOLD HELPER FUNCTIONS BEGIN HERE */
+void yT(ifstream& input, vector<Thresh>& y, int col) {
+	// declaring stringstream variables
+	string line, word;
+
+	// takes in each line, reads garbage numbers up to column number
+	// takes the column we want and compares to prev max/min and resets
+	// if need be
+	while(getline(input, line)) {
+		stringstream ss;
+
+		// discarding garbage values
+		for(int i=0; i<col; i++) {
+			string temp;
+			ss << line;
+			ss >> temp;
+		}
+
+		// converting column string to double
+		ss << line;
+		ss >> word;
+		stringstream tt;
+		tt << word;
+		double val;
+		tt >> val;
+
+		Thresh v(val);
+
+		y.push_back(v);
+	}
+}
+
+void swap(vector<Thresh>& v, int x, int z) {
+    double ty = v[x].y;
+    double tt = v[x].time;
+    double td = v[x].derivative;
+    v[x].y = v[z].y;
+    v[x].time = v[z].time;
+    v[x].derivative = v[z].derivative;
+    v[z].y = ty;
+    v[z].time = tt;
+    v[z].derivative = td;
 
 }
+
+void quicksortY(vector<Thresh> &vec, int L, int R) {
+    int i, j, mid; 
+    double piv;
+    i = L;
+    j = R;
+    mid = L + (R - L) / 2;
+    piv = abs(vec[mid].y);
+
+    while (i<R || j>L) {
+        while (abs(vec[i].y) < piv)
+            i++;
+        while (abs(vec[j].y) > piv)
+            j--;
+
+        if (i <= j) {
+            swap(vec, i, j);
+            i++;
+            j--;
+        }
+        else {
+            if (i < R)
+                quicksortY(vec, i, R);
+            if (j > L)
+                quicksortY(vec, L, j);
+            return;
+        }
+    }
+}
+
+void quicksortT(vector<Thresh> &vec, int L, int R) {
+    int i, j, mid; 
+    double piv;
+    i = L;
+    j = R;
+    mid = L + (R - L) / 2;
+    piv = abs(vec[mid].time);
+
+    while (i<R || j>L) {
+        while (abs(vec[i].time) < piv)
+            i++;
+        while (abs(vec[j].time) > piv)
+            j--;
+
+        if (i <= j) {
+            swap(vec, i, j);
+            i++;
+            j--;
+        }
+        else {
+            if (i < R)
+                quicksortT(vec, i, R);
+            if (j > L)
+                quicksortT(vec, L, j);
+            return;
+        }
+    }
+}
+
+vector<Thresh> window(vector<Thresh>& v, double w, int peaks) {
+	vector<Thresh> final;
+
+	int count = static_cast<int>(v.size()-1);
+	final.push_back(v[count--]);
+	while((final.size() < peaks) && (count > 0)) {
+		bool good = true;
+		for(unsigned int i=0; i<final.size();i++) {
+			if(abs(abs(v[count].time) - abs(final[i].time)) < w) {
+				good = false;
+			}
+		}
+		if(good) {
+			final.push_back(v[count]);
+			count--;
+		} else {
+			count--;
+		}
+	}
+	return final;
+}
+
+double dist(vector<Thresh>& v) {
+	double sum = 0;
+	double prev = v[0].time;
+	for(unsigned int i=1; i<v.size(); i++) {
+		double diff = abs(prev-v[i].time);
+		sum += diff;
+		prev = v[i].time;
+	}
+
+	return sum/(static_cast<int>(v.size()-1));
+}
+
+void neg(vector<Thresh>& v) {
+	vector<Thresh> final;
+	for(unsigned int i=0; i<v.size(); i++) {
+		if(v[i].y > 0) {
+			final.push_back(v[i]);
+		}
+	}
+
+	v = final;
+}
+
+double max(vector<Thresh>& v) {
+	double max = 0;
+	for(unsigned int i=1; i<v.size(); i++) {
+		double step = v[i].time - v[i-1].time;
+		if(step > max) {
+			max = step;
+		}
+	}
+	return max;
+}
+
+double min(vector<Thresh>& v) {
+	double min = 10000000;
+	for(unsigned int i=1; i<v.size(); i++) {
+		double step = v[i].time - v[i-1].time;
+		if(step < min) {
+			min = step;
+		}
+	}
+	return min;
+	
+}
+
+double avg(vector<Thresh>& v) {
+	double sum = 0;
+	int count = 0;
+	for(unsigned int i=1; i<v.size(); i++) {
+		double step = v[i].time - v[i-1].time;
+		sum += step;
+		count++;
+	}
+	return sum/count;
+}
+
+double miny(vector<Thresh>& v) {
+	double min = 10000000;
+	for(unsigned int i=0; i<v.size(); i++) {
+		if(v[i].y < min) {
+			min = v[i].y;
+		}
+	}
+	return min;
+	
+}
+
+void formatT(ofstream& output, vector<Thresh>& v) {
+	for(unsigned int i=0; i<v.size(); i++) {
+		output << v[i].time << " - " << v[i].y << " - " << v[i].derivative << endl;
+	}
+}
+
+/* *********** END OF THRESHOLD HELPER FUNCTIONS **************** */
+
+
+
+/* THRESHOLD FUNCTION TO DISCOVER THE THRESHOLD AFTER INPUTTING PEAK # */
+void threshold(bool oculus, ofstream& output, ifstream& input, ifstream& tinput, int peaks) {
+		vector<Thresh> ys;
+
+		if(oculus) {
+			time(tinput, ys);
+		} else {
+			gtime(tinput, ys);
+		}
+
+		yT(input, ys, ycol);
+		neg(ys);
+		quicksortY(ys, 0, static_cast<int>(ys.size()-1));
+		vector<Thresh> sm = window(ys, win, peaks);
+		quicksortT(sm, 0, static_cast<int>(sm.size()-1));
+
+		// output 
+		output << "Time before considering " << arg2 << " threshold: " << min(sm) - 0.02 << endl;
+		output << "Acceleration threshold for " << arg2 << ": " << miny(sm) - 0.3 << endl;
+		formatT(output, sm);
+
+}
+/* ******************************************************************* */
+
+
 
 // basically will do the three S, M, F and put them in an array
 // then will go through
@@ -221,27 +453,21 @@ int main(int argc, char* argv[]) {
 	}
 /* ********************************** */
 
-
-// FIRST THING WE NEED TO DO IS CLEAN THE DATA AND PLOT IT
-	// MAYBE DO THIS IN ANOTHER PROGRAM THAT SPITS IT OUT IN ODATA GSATA SLOW MEDIUM FAST
-
-// CREATE AND CALL PYTHON SCRIPT TO PLOT THE DATA
-
-// IN A LOOP ???
+for(unsigned int i=0; i<file.size(); i++) {
 	ifstream pyin(inputs[i]);
 
-/* SEND STANDARD HEADER TEMPLATE TO PYTHON FILE */
+	/* SEND STANDARD HEADER TEMPLATE TO PYTHON FILE */
 	string ppath = "plot" + i + ".py";
 	ofstream pyout(ppath);
 	pyout << "import matplotlib.pyplot as plt" << endl;
 
-/* ******************************************** */
+	/* ******************************************** */
 
-/* PERFORM ANALYSIS ON THE FILE */
+	/* PERFORM ANALYSIS ON THE FILE */
 	python(oculus, ycol, pyout, pyin);
 
 
-/* SEND STANDARD FOOTER TEMPLATE TO PYTHON FILE */
+	/* SEND STANDARD FOOTER TEMPLATE TO PYTHON FILE */
 	pyout << "plt.plot(t,y)" << endl;
 	pyout << "plt.ylabel('acceleration')" << endl;
 
@@ -249,129 +475,33 @@ int main(int argc, char* argv[]) {
 	pyout << "plt.show()" << endl;
 	pyout << "plt.close()" << endl;
 
-/* ********************************************* */
+	/* ********************************************* */
 
 
-/* CALL THE PYTHON SCRIPT FROM HERE */
-	string command = "python \"" + pplot + "\"";
-	system(command);
-/* ******************************** */
+	/* CALL THE PYTHON SCRIPT FROM HERE */
+		string command = "python \"" + pplot + "\"";
+		system(command);
+	/* ******************************** */
 
-/* ASK USER FOR NUMBER OF PEAKS AND USE AS NUM */
+	/* ASK USER FOR NUMBER OF PEAKS AND USE AS NUM */
+	cout << "Number of peaks? " << endl;
+	string speaks;
+	cin >> speaks;
+	stringstream ss;
+	ss << speaks;
+	int peaks;
+	ss >> peaks;
+	/* ******************************************* */
 
 
-
-
-
-
-/* ******************************************* */
-
-
-/* THRESHOLD ANALYSIS - should write thresholds to a file */
+	/* THRESHOLD ANALYSIS - should write thresholds to a file */
 	ofstream output(outputs[i]);
 	ifstream input(inputs[i]);
+	ifstream tinput(inputs[i]);
 
-	threshold(oculus, output, input);
-	// DO ALL OF THIS ALL OF THIS ALL OF THIS AND PUT IT IN A LOOP
-
-
-/* ****************** */
-
-
-
-
-	// iterates through all files and performs the analysis
-	for(unsigned int j=0; j<SIZE; j++) {
-	// iterating through columns from col input --> final column
-		vector<Thresh> ys;
-
-		ofstream output(outputs[j]);
-
-		ifstream input(inputs[j]);
-
-
-		ifstream input1(inputs[j]);
-		y(input1, ys, ycol);
-
-		if(oculus) {
-			time(input, ys);
-		} else {
-			gtime(input, ys);
-		}
-
-		neg(ys);
-
-		// derivative(ys);
-		// cout << "d: " << dist(ys) << endl;
-
-
-		quicksortY(ys, 0, static_cast<int>(ys.size()-1));
-
-
-		// use diff window thresholds for the slow, medium, fast
-		vector<Thresh> sm = window(ys, win, peaks);
-
-		quicksortT(sm, 0, static_cast<int>(sm.size()-1));
-		cout << "small: " << dist(sm) << endl;
-
-		for(unsigned int i=0; i<sm.size(); i++) {
-			cout << sm[i].time << endl;
-		}
-
-		cout << "MAX: " << max(sm) << endl;
-		cout << "MIN: " << min(sm) << endl;
-		cout << "AVG: " << avg(sm) << endl;
-
-		cout << miny(sm) << endl;
-
-		// threshold for time should be min time - .06 for error
-		// so this would be 0.58 for slow
-		
-		// or look at avg of the bottom half% and then subtract for error
-		// NEED MORE SLOW MEDIUM FAST DATA TO TEST THIS ON
-
-
-		vector<Thresh> final = sm;
-		// int len = static_cast<int>(final.size());
-		// double start = 0.2;
-		// double end = static_cast<double>(dist(final));
-
-
-		// int SIZE = 10;
-		// int count = 0;
-		// vector<Thresh> temp;
-
-		// while ((len != SIZE) && count < 100) {
-
-		// 	double distance = (end-start)/2 + start;
-
-		// 	temp = window(final, static_cast<double>(dist(final)));
-		// 	len = static_cast<int>(temp.size());
-
-		// 	if(len > SIZE) {
-		// 		start = distance;
-		// 	} else {
-		// 		end = distance;
-		// 	}
-		// 	count++;
-		// }
-
-		// cout << count << endl;
-		// final = temp;
-
-		// for any that are under the average time different (or some percentage of it) then remove those and if
-		// the size of the new vector is below whatever we want it to be then add back the most recent ones
-
-		// vector<Thresh> xs = window(sm, static_cast<double>(dist(sm)));
-
-		// create temp vector and assign 
-
-		cout << "Time before considering " << arg2 << " threshold: " << min(sm) - 0.02 << endl;
-		cout << "Acceleration threshold for " << arg2 << ": " << miny(sm) - 0.3 << endl;
-		format(output, final);
-
-		// quicksort y
-	}
+	threshold(oculus, output, input, tinput, peaks);
+	/* ***************************************************** */
+}
 
 	// frees memory
 	delete [] inputs;
